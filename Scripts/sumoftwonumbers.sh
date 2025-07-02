de8afd3fe139bd298bbb491232d451b9e79da352
monu1398 <113309592+Mohanvamsi13@users.noreply.github.com>
2025-07-02 14:29:29 -0500

    Commiting sumoftwonumbers.sh

---
+++ Scripts/sumoftwonumbers.sh
@@ -0,0 +1,19 @@
+###############################################################
+
+#Name: Mohan Vamsi Dasam
+#Date: 07-02-2025
+#Purpose: Basic shell script to execute sum of two numbers
+
+##############################################################
+
+
+#!/bin/bash
+
+read -p "Enter the first number: " num1
+
+read -p "Enter the second number " num2
+
+sum=$((num1+num2))
+
+echo "Sum of two numbers: $sum"
+
